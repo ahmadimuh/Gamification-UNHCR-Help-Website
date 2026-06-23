049cd9175f17f232eca9e9e3b9ac807efb11ddb7
mohammad ahmadi <131284248+ahmadimuh@users.noreply.github.com>
2026-06-23 15:52:52 +0330

    v.1.0.2

--- includes/format/text.php
+++ includes/format/text.php
@@ -7,7 +7,7 @@ if ($char1=='on' && $char2=='on') { $char1_class = 'char-left';$char2_class = 'c
 ?>
 
 
-<div id="un-stage-dialogue" class="dialogue-scene typing-effect" <?php if ($bg_image) { ?>style="background-image: url('<?php echo $bg_image;?>')" <?php } ?> >
+<div id="un-stage-dialogue" class="dialogue-scene " <?php if ($bg_image) { ?>style="background-image: url('<?php echo $bg_image;?>')" <?php } ?> >
 <div class="next-level-alert" >
 <svg width="140px" height="140px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="next-level-alert-icon" >
 <path fill-rule="evenodd" clip-rule="evenodd" d="M5.60439 4.23093C4.94586 3.73136 4 4.20105 4 5.02762V18.9724C4 19.799 4.94586 20.2686 5.60439 19.7691L14.7952 12.7967C15.3227 12.3965 15.3227 11.6035 14.7952 11.2033L5.60439 4.23093ZM2 5.02762C2 2.54789 4.83758 1.13883 6.81316 2.63755L16.004 9.60993C17.5865 10.8104 17.5865 13.1896 16.004 14.3901L6.81316 21.3625C4.83758 22.8612 2 21.4521 2 18.9724V5.02762Z" fill="#0F0F0F"></path>
