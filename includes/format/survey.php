d5dc6e8eafe9aae81bad5b50ecdf9d734de374c6
mohammad ahmadi <131284248+ahmadimuh@users.noreply.github.com>
2026-06-23 15:55:03 +0330

    v.1.0.2

--- includes/format/survey.php
+++ includes/format/survey.php
@@ -1,17 +1,17 @@
-<div class="un-stage un-stage-type-quiz" data-correct="<?php echo $stage_id;?>" data-wrong="0" data-neutral="0">
+<div class="un-stage un-stage-type-quiz" data-correct="<?php echo $stage_id;?>" data-stage-id="1" data-category="survey" data-wrong="0" data-neutral="0">
 
 		
 <div id="un-stage-dialogue" class="dialogue-scene dialogue-quiz typing-effect" style="background-image: url('<?php echo $bg_image; ?>')">
 
     <!-- سؤال ۱: سن -->
-    <div class="un-question-block" id="question-age">
+    <div class="un-question-block" id="question-age" >
         <div class="un-quiz-question">شما در چه رنج سنی قرار دارید؟</div>
         <ul class="un-quiz-options">
-            <li class="un-quiz-option" data-value="under_10">🔵 کمتر از 10 سال</li>
-            <li class="un-quiz-option" data-value="10_20">🔵 بین 10 تا 20 سال</li>
-            <li class="un-quiz-option" data-value="20_30">🔵 بین 20 تا 30 سال</li>
-            <li class="un-quiz-option" data-value="30_40">🔵 بین 30 تا 40 سال</li>
-            <li class="un-quiz-option" data-value="over_40">🔵 بالای 40 سال</li>
+            <li class="un-quiz-option" data-value="under_10"> کمتر از 10 سال</li>
+            <li class="un-quiz-option" data-value="10_20"> بین 10 تا 20 سال</li>
+            <li class="un-quiz-option" data-value="20_30"> بین 20 تا 30 سال</li>
+            <li class="un-quiz-option" data-value="30_40"> بین 30 تا 40 سال</li>
+            <li class="un-quiz-option" data-value="over_40"> بالای 40 سال</li>
         </ul>
     </div>
 
@@ -19,8 +19,8 @@
     <div class="un-question-block" id="question-gender" style="display: none;">
         <div class="un-quiz-question">جنسیت شما چیست؟</div>
         <ul class="un-quiz-options">
-            <li class="un-quiz-option" data-value="female">🔵 زن</li>
-            <li class="un-quiz-option" data-value="male">🔵 مرد</li>
+            <li class="un-quiz-option" data-value="female"> زن</li>
+            <li class="un-quiz-option" data-value="male"> مرد</li>
         </ul>
     </div>
 
